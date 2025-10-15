2c7a744c4f623d9c1dcb18ad953c59dd6639fcbe
lrtwithlesley <144685229+lrtwithlesley@users.noreply.github.com>
2025-10-15 23:31:17 +0800

    Add files via upload

---
+++ music.sql
@@ -0,0 +1,25 @@
+CREATE DATABASE music_app;
+GO
+
+USE music_app;
+GO
+
+CREATE TABLE users (
+  id INT IDENTITY(1,1) PRIMARY KEY,
+  username NVARCHAR(50) NOT NULL,
+  password NVARCHAR(100) NOT NULL
+  avatar NVARCHAR(200) NULL;
+);
+
+CREATE TABLE favorites (
+    id INT IDENTITY(1,1) PRIMARY KEY,
+    userId INT NOT NULL,
+    musicId BIGINT NOT NULL,
+    createdAt DATETIME DEFAULT GETDATE()
+);
+CREATE TABLE history (
+    id INT IDENTITY(1,1) PRIMARY KEY,
+    userId INT NOT NULL,
+    musicId BIGINT NOT NULL,
+    playedAt DATETIME DEFAULT GETDATE()
+);
