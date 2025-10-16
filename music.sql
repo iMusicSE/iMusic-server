34a72a5c6947a7024d44bbfa4f5de35519fbf5b6
RheinXenon <2458192495@qq.com>
2025-10-16 13:17:53 +0800

    修正music.sql语法错误、修改README.md和package.json准确依赖、规范化文件结构

--- music.sql
+++ music.sql
@@ -7,8 +7,8 @@ GO
 CREATE TABLE users (
   id INT IDENTITY(1,1) PRIMARY KEY,
   username NVARCHAR(50) NOT NULL,
-  password NVARCHAR(100) NOT NULL
-  avatar NVARCHAR(200) NULL;
+  password NVARCHAR(100) NOT NULL,
+  avatar NVARCHAR(200) NULL
 );
 
 CREATE TABLE favorites (
