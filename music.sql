e11db8945424c8352f4b65fc44c615fc19519431
lrtwithlesley <144685229+lrtwithlesley@users.noreply.github.com>
2025-10-19 13:54:56 +0800

    增加下载表
    Added DownloadedSongs table with relevant fields.

--- music.sql
+++ music.sql
@@ -22,4 +22,20 @@ CREATE TABLE history (
     userId INT NOT NULL,
     musicId BIGINT NOT NULL,
     playedAt DATETIME DEFAULT GETDATE()
+
+);
+
+CREATE TABLE DownloadedSongs (
+    downloadId INT IDENTITY(1,1) PRIMARY KEY,  -- 自增主键
+    userId INT NULL,                           -- 关联用户（可选）
+    musicId BIGINT NOT NULL,                    -- 歌曲ID
+    songName NVARCHAR(200) NOT NULL,           -- 歌曲名
+    artist NVARCHAR(200) NULL,                 -- 歌手
+    album NVARCHAR(200) NULL,                  -- 专辑
+    coverUrl VARCHAR(500) NULL,                -- 封面图URL
+    localPath VARCHAR(500) NULL,               -- 本地保存路径
+    downloadTime DATETIME DEFAULT GETDATE(),   -- 下载时间
+    status INT DEFAULT 1                       -- 状态：1=已下载,0=失败
+	  lyricsPath VARCHAR(500) NULL,    -- 歌词文件
+	  lyricsStatus INT DEFAULT 0;   
 );
